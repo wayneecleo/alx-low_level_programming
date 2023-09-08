023beb333bfcf2a7fcaa6c0eca3b3a27ee8a67ef
wayneecleo <wayneecleo@gmail.com>
2023-09-07 19:28:34 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 /**
- 	* main - this prints exactly the input on it
- * Return: always 1.
- */
+* main - this prints exactly the input on it
+* Return: always 1.
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
