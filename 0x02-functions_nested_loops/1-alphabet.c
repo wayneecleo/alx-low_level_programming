28df909082bc7de67f3a02754629b118db99d28e
wayneecleo <wayneecleo@gmail.com>
2023-09-13 17:07:39 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print alpabets- function to print alphabet in lower case
+*return: O
+*/
+
+ void print_alphabet(void)
+{
+	char i;
+
+	for  (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
